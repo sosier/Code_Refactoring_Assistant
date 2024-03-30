3f13eb0c79a063f9b5e8f359a980579fa0bba80e
Sean Osier <sean@seanosier.com>
2024-03-29 22:51:04 -0400

    Add safe_eval files

---
+++ safe_eval/safe_eval.py
@@ -0,0 +1,286 @@
+"""
+Modified from OpenAI's HumanEval dataset evaluation code here:
+https://github.com/openai/human-eval/blob/master/human_eval/execution.py
+(Retrieved 2024-03-17)
+
+Changes include:
+ - Removing unused imports and alphabetize
+ - Generalize check_correctness to check that any input code satisfies any
+   input test_code, so that function is no longer HumanEval specific
+ - Removing multiprocessing and related code in check_correctness for simplicity
+   and becuase it was causing bugs (It's okay if code doesn't run in parallel)
+ - Adding ability to call check_correctness from command line
+"""
+import argparse
+import contextlib
+import faulthandler
+import io
+import json
+import os
+import platform
+import signal
+import sys
+import tempfile
+from timeit import Timer
+from typing import Optional, Dict
+
+
+# problem: Dict, completion: str
+def check_correctness(
+        code: str,
+        test_code: str,
+        evaluate_run_time: Optional[bool] = False,
+        timeout: float = 10.0  # seconds
+) -> Dict:
+    """
+    Evaluates the functional correctness of a piece of code by running the
+    provided test code
+
+    :param task_id: an optional ID for the specific coding task
+    :param generation_id: an optional ID for the specific piece of generated
+        code for the coding task
+    :param timeout: maximum seconds code is allowed to run before we force the
+        execution to timeout
+    """
+    result = None
+    compiled = False
+    passed_tests = False
+    avg_test_time = None
+
+    # Unsafe Execute:
+    with create_tempdir():
+
+        # These system calls are needed when cleaning up tempdir.
+        import os
+        import shutil
+        rmtree = shutil.rmtree
+        rmdir = os.rmdir
+        chdir = os.chdir
+
+        # Disable functionalities that can make destructive changes to the test.
+        reliability_guard()
+
+        # Construct the check program and run it.
+        check_program = code + "\n\n" + test_code
+
+        try:
+            exec_globals = {}
+            with swallow_io():
+                with time_limit(timeout):
+# WARNING
+# This program exists to execute untrusted model-generated code. Although
+# it is highly unlikely that model-generated code will do something overtly
+# malicious in response to this test suite, model-generated code may act
+# destructively due to a lack of model capability or alignment.
+# Users are strongly encouraged to sandbox this evaluation suite so that it 
+# does not perform destructive actions on their host or network. For more 
+# information on how OpenAI sandboxes its code, see the accompanying paper.
+# Once you have read this disclaimer and taken appropriate precautions, 
+# uncomment the following line and proceed at your own risk:
+                    exec(code, exec_globals)
+                    compiled = True
+                    exec(check_program, exec_globals)
+                    passed_tests = True
+            
+                if evaluate_run_time:
+                    # Using of min of timing runs at suggestion of timeit
+                    # package authors:
+                    avg_test_time = min(
+                        Timer(
+                            lambda: exec(test_code, exec_globals)
+                        ).repeat(3, 1000)
+                    ) / 1000
+
+            result = "passed"
+        except TimeoutException:
+            result = "timed out"
+        except BaseException as e:
+            result = f"failed: {e}"
+
+        # Needed for cleaning up.
+        shutil.rmtree = rmtree
+        os.rmdir = rmdir
+        os.chdir = chdir
+
+    
+    if not result:
+        result = "timed out"
+
+    return dict(
+        result=result,
+        compiled=compiled,
+        passed_tests=passed_tests,
+        **(dict(avg_test_time=avg_test_time) if evaluate_run_time else {})
+    )
+
+
+@contextlib.contextmanager
+def time_limit(seconds: float):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.setitimer(signal.ITIMER_REAL, seconds)
+    signal.signal(signal.SIGALRM, signal_handler)
+    try:
+        yield
+    finally:
+        signal.setitimer(signal.ITIMER_REAL, 0)
+
+
+@contextlib.contextmanager
+def swallow_io():
+    stream = WriteOnlyStringIO()
+    with contextlib.redirect_stdout(stream):
+        with contextlib.redirect_stderr(stream):
+            with redirect_stdin(stream):
+                yield
+
+
+@contextlib.contextmanager
+def create_tempdir():
+    with tempfile.TemporaryDirectory() as dirname:
+        with chdir(dirname):
+            yield dirname
+
+
+class TimeoutException(Exception):
+    pass
+
+
+class WriteOnlyStringIO(io.StringIO):
+    """ StringIO that throws an exception when it's read from """
+
+    def read(self, *args, **kwargs):
+        raise IOError
+
+    def readline(self, *args, **kwargs):
+        raise IOError
+
+    def readlines(self, *args, **kwargs):
+        raise IOError
+
+    def readable(self, *args, **kwargs):
+        """ Returns True if the IO object can be read. """
+        return False
+
+
+class redirect_stdin(contextlib._RedirectStream):  # type: ignore
+    _stream = 'stdin'
+
+
+@contextlib.contextmanager
+def chdir(root):
+    if root == ".":
+        yield
+        return
+    cwd = os.getcwd()
+    os.chdir(root)
+    try:
+        yield
+    except BaseException as exc:
+        raise exc
+    finally:
+        os.chdir(cwd)
+
+
+def reliability_guard(maximum_memory_bytes: Optional[int] = None):
+    """
+    This disables various destructive functions and prevents the generated code
+    from interfering with the test (e.g. fork bomb, killing other processes,
+    removing filesystem files, etc.)
+
+    WARNING
+    This function is NOT a security sandbox. Untrusted code, including, model-
+    generated code, should not be blindly executed outside of one. See the 
+    Codex paper for more information about OpenAI's code sandbox, and proceed
+    with caution.
+    """
+
+    if maximum_memory_bytes is not None:
+        import resource
+        resource.setrlimit(resource.RLIMIT_AS, (maximum_memory_bytes, maximum_memory_bytes))
+        resource.setrlimit(resource.RLIMIT_DATA, (maximum_memory_bytes, maximum_memory_bytes))
+        if not platform.uname().system == 'Darwin':
+            resource.setrlimit(resource.RLIMIT_STACK, (maximum_memory_bytes, maximum_memory_bytes))
+
+    faulthandler.disable()
+
+    import builtins
+    builtins.exit = None
+    builtins.quit = None
+
+    import os
+    os.environ['OMP_NUM_THREADS'] = '1'
+
+    os.kill = None
+    os.system = None
+    os.putenv = None
+    os.remove = None
+    os.removedirs = None
+    os.rmdir = None
+    os.fchdir = None
+    os.setuid = None
+    os.fork = None
+    os.forkpty = None
+    os.killpg = None
+    os.rename = None
+    os.renames = None
+    os.truncate = None
+    os.replace = None
+    os.unlink = None
+    os.fchmod = None
+    os.fchown = None
+    os.chmod = None
+    os.chown = None
+    os.chroot = None
+    os.fchdir = None
+    os.lchflags = None
+    os.lchmod = None
+    os.lchown = None
+    os.getcwd = None
+    os.chdir = None
+
+    import shutil
+    shutil.rmtree = None
+    shutil.move = None
+    shutil.chown = None
+
+    import subprocess
+    subprocess.Popen = None  # type: ignore
+
+    # Commenting this out as incluidng it causes issues when running from
+    # command line:
+    # __builtins__['help'] = None
+
+    import sys
+    sys.modules['ipdb'] = None
+    sys.modules['joblib'] = None
+    sys.modules['resource'] = None
+    sys.modules['psutil'] = None
+    sys.modules['tkinter'] = None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Safetly check the correctness and run time of code"
+    )
+    parser.add_argument("code", help="Code to evaluate")
+    parser.add_argument("test_code", help="Tests to run")
+    parser.add_argument(
+        "--timeout",
+        type=float,
+        default=10.0,
+        help="Timeout for execution in seconds (default: 10.0)"
+    )
+    args = parser.parse_args()
+
+    # For debug:
+    # print(args.code)
+    # print(args.test_code)
+    # print(args.timeout)
+
+    # Call the check_correctness function with provided arguments
+    print(json.dumps(check_correctness(
+        args.code,
+        args.test_code,
+        evaluate_run_time=True,
+        timeout=args.timeout
+    )))
